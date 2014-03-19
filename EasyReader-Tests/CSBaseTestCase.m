eb2e67a01c2911496e03d7f17bc776106f24085e
Joey Lorich <joey@cloudspace.com>
2014-03-19 16:27:37 -0400

    Add base test

---
+++ EasyReader-Tests/CSBaseTestCase.m
@@ -0,0 +1,14 @@
+//
+//  CSBaseTestCase.m
+//  EasyReader
+//
+//  Created by Joseph Lorich on 3/19/14.
+//  Copyright (c) 2014 Cloudspace. All rights reserved.
+//
+
+#import "CSBaseTestCase.h"
+
+
+@implementation CSBaseTestCase
+
+@end
