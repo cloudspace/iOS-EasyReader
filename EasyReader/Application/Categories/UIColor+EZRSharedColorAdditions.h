865f955f8a999fdde7d64d7112932e6d56338bbc
Michael Beattie <beattie@cloudspace.com>
2014-03-24 12:12:51 -0400

    [#67767620] Minimized lines of code to set imageView

---
+++ EasyReader/Application/Categories/UIColor+EZRSharedColorAdditions.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+EZRSharedColorAdditions.h
+//  EasyReader
+//
+//  Created by Michael Beattie on 3/24/14.
+//  Copyright (c) 2014 Cloudspace. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIColor_EZRSharedColorAdditions : NSObject
+
+@end
---
+++ EasyReader/Application/Categories/UIColor+EZRSharedColorAdditions.m
@@ -0,0 +1,13 @@
+//
+//  UIColor+EZRSharedColorAdditions.m
+//  EasyReader
+//
+//  Created by Michael Beattie on 3/24/14.
+//  Copyright (c) 2014 Cloudspace. All rights reserved.
+//
+
+#import "UIColor+EZRSharedColorAdditions.h"
+
+@implementation UIColor_EZRSharedColorAdditions
+
+@end
