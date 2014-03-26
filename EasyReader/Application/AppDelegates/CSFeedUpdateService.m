52a9639431430a20a8139558aaeabec697887cd9
Michael Beattie <beattie@cloudspace.com>
2014-03-26 13:09:21 -0400

    [#67871860] modified to keeps feeds at only 10 feed items each

--- EasyReader/Application/AppDelegates/CSFeedUpdateService.m
+++ EasyReader/Application/AppDelegates/CSFeedUpdateService.m
@@ -8,6 +8,9 @@
 
 #import "CSFeedUpdateService.h"
 #import "CSFeedItemUpdater.h"
+#import "User.h"
+#import "Feed.h"
+#import "FeedItem.h"
 
 @implementation CSFeedUpdateService
 
@@ -21,7 +24,38 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
+    User *currentUser = [User current];
     
+    for (Feed *feed in currentUser.feeds) {
+        
+        // Limit feed items to 10
+        if (feed.feedItems.count > 10) {
+            NSMutableArray *sortedFeedItems = [[NSMutableArray alloc] init];
+            sortedFeedItems = [self sortFeedItems:feed.feedItems];
+            while (sortedFeedItems.count > 10) {
+                
+                // Delete the oldest feed item
+                FeedItem *toDelete = [sortedFeedItems objectAtIndex:sortedFeedItems.count-1];
+                [toDelete deleteEntity];
+                [sortedFeedItems removeObjectAtIndex:sortedFeedItems.count-1];
+                [[NSManagedObjectContext defaultContext] saveToPersistentStoreAndWait];
+            }
+        }
+    }
+}
+
+/**
+ * Sort feed items by date descending
+ */
+- (NSMutableArray *)sortFeedItems:(NSSet *)feedItems
+{
+    NSArray *sortableArray = [NSArray arrayWithArray:[feedItems allObjects]];
+    
+    NSSortDescriptor *sortDescriptor;
+    sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"updatedAt"
+                                                 ascending:NO];
+    NSArray *sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
+    return [NSMutableArray arrayWithArray:[sortableArray sortedArrayUsingDescriptors:sortDescriptors]];
 }
 
 @end
